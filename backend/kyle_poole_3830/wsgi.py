73418e843a3db5102dbe029dca617da53ba5b209
Crowdbotics <team@crowdbotics.com>
2019-05-25 14:56:34 +0000

    initial commit

---
+++ backend/kyle_poole_3830/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for kyle_poole_3830 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "kyle_poole_3830.settings")
+
+application = get_wsgi_application()
